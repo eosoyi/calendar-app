b6e2e696ee582e5b2648d50845fdddb525f885a3
eosoyi <eosoyi@outlook.com>
2024-07-26 08:26:51 -0600

    first commit create app

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,52 @@
+import { Navbar } from "../components/Navbar";
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import enUS from "date-fns/locale/en-US";
+import { addHours } from "date-fns";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Cumpleaños jefe",
+    notes: "Hay que comprar un pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Erick",
+    },
+  },
+];
+
+export const CalendarPage = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
+    </>
+  );
+};
