8f4c77c9f57afcb01ea8195d7b7596d5af28b534
eosoyi <eosoyi@outlook.com>
2024-08-01 08:29:06 -0600

    se inicia con la creacion del calendario y modal

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,26 +1,15 @@
 import { Navbar } from "../components/Navbar";
 
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
-import enUS from "date-fns/locale/en-US";
+import { Calendar } from "react-big-calendar";
 import { addHours } from "date-fns";
 
+import { localizer } from "../../helpers/CalendarLocalizer";
+import { getMessagesEs } from "../../helpers/getMessages";
+import { CalendarEvent } from "../components/CalendarEvent";
+
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
-const locales = {
-  "en-US": enUS,
-};
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
+import { useState } from "react";
+import { CalendarModal } from "../components/CalendarModal";
 
 const events = [
   {
@@ -37,16 +26,56 @@ const events = [
 ];
 
 export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "week"
+  );
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#347CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+
+    return { style };
+  };
+
+  const onDoubleClick = (event) => {
+    window.alert(JSON.stringify(event));
+  };
+
+  const onSelect = (event) => {
+    console.log({ click: event });
+  };
+
+  const onViewChange = (event) => {
+    console.log({ viewChange: event });
+    localStorage.setItem("lastView", event);
+    setLastView(event);
+  };
+
   return (
     <>
       <Navbar></Navbar>
       <Calendar
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 'calc(100vh - 80px)' }}
+        style={{ height: "calc(100vh - 80px)" }}
+        culture="es"
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent,
+        }}
+        onSelectEvent={onSelect}
+        onDoubleClickEvent={onDoubleClick}
+        onView={onViewChange}
       />
+      <CalendarModal></CalendarModal>
     </>
   );
 };
