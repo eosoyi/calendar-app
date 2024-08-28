c8919b025fbee302effd0968eb389ecada181b60
eosoyi <eosoyi@outlook.com>
2024-08-27 23:31:50 -0600

    Se aplica logica para conectar nuestro login con el backend y poder crear y autenticar usuarios

---
+++ src/hooks/useAuthStore.js
@@ -0,0 +1,83 @@
+import { useDispatch, useSelector } from "react-redux";
+import calendarApi from "../api/calendarApi";
+import {
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogout,
+} from "../store/auth/authSlice";
+
+export const useAuthStore = () => {
+  const { status, user, errorMessage } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const startLogin = async ({ email, password }) => {
+    dispatch(onChecking());
+    try {
+      const { data } = await calendarApi.post("auth", { email, password });
+      console.log(data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      console.log(error);
+      dispatch(onLogout("Credenciales incorrectas"));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
+    }
+  };
+
+  const startRegister = async ({ email, password, name }) => {
+    dispatch(onChecking());
+    try {
+      const { data } = await calendarApi.post("auth/new", {
+        email,
+        password,
+        name,
+      });
+      console.log(data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      console.log("error = ", error.response.data);
+      dispatch(onLogout(error.response.data?.msg || "---"));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
+    }
+  };
+
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return dispatch(onLogout());
+
+    try {
+      const { data } = await calendarApi.get("auth/renew");
+      console.log(data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      localStorage.clear();
+      dispatch(onLogout());
+    }
+  };
+
+  const startLogout = () => {
+    localStorage.clear();
+    dispatch(onLogout());
+  };
+
+  return {
+    status,
+    user,
+    errorMessage,
+
+    startLogin,
+    startRegister,
+    checkAuthToken,
+    startLogout
+  };
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,34 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { CalendarPage } from "../calendar/pages/CalendarPage";
+import { getEnvVariables } from "../helpers/getEnvVariables";
+import { useAuthStore } from "../hooks/useAuthStore";
+import { useEffect } from "react";
 
 export const AppRouter = () => {
-  //const authStatus = "not-authenticated";
-  const authStatus = "authenticated";
+  const { status, checkAuthToken } = useAuthStore();
+
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
+  if (status === "checking") {
+    return <h3>Cargando...</h3>;
+  }
+
   return (
     <Routes>
-      {authStatus === "not-authenticated" ? (
-        <Route path="/auth/*" element={<LoginPage></LoginPage>} />
+      {status === "not-authenticated" ? (
+        <>
+          <Route path="/auth/*" element={<LoginPage></LoginPage>} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
       ) : (
-        <Route path="/*" element={<CalendarPage></CalendarPage>} />
+        <>
+          <Route path="/*" element={<CalendarPage></CalendarPage>} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
       )}
-
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
