4dc47ba6e1492d9c996fb8f8a710fccc919db771
eosoyi <eosoyi@outlook.com>
2024-09-27 21:49:30 -0600

    se finaliza con el crud de los eventos en el calendar store

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,30 +1,32 @@
 import { Navbar } from "../components/Navbar";
 
 import { Calendar } from "react-big-calendar";
-import { addHours } from "date-fns";
 
 import { localizer } from "../../helpers/CalendarLocalizer";
 import { getMessagesEs } from "../../helpers/getMessages";
 import { CalendarEvent } from "../components/CalendarEvent";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CalendarModal } from "../components/CalendarModal";
 import { useUiStore } from "../../hooks/useUiStore";
 import { useCalendarStore } from "../../hooks/useCalendarStore";
 import { FabAddNew } from "../components/FabAddNew";
 import { FabDelete } from "../components/FabDelete";
+import { useAuthStore } from "../../hooks/useAuthStore";
 
 export const CalendarPage = () => {
+  const { user } = useAuthStore();
   const { openDateModal } = useUiStore();
-  const { events, setActiveEvent } = useCalendarStore();
+  const { events, setActiveEvent, startLoadingEvent } = useCalendarStore();
   const [lastView, setLastView] = useState(
     localStorage.getItem("lastView") || "week"
   );
 
   const eventStyleGetter = (event, start, end, isSelected) => {
+    const isMyEvent = (user.uid === event.user._id) || (user.uid === event.user.uid); 
     const style = {
-      backgroundColor: "#347CF7",
+      backgroundColor: isMyEvent ? "#347CF7" : "#465660",
       borderRadius: "0px",
       opacity: 0.8,
       color: "white",
@@ -46,6 +48,10 @@ export const CalendarPage = () => {
     localStorage.setItem("lastView", event);
     setLastView(event);
   };
+
+  useEffect(() => {
+    startLoadingEvent();
+  }, []);
 
   return (
     <>
--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -4,36 +4,64 @@ import {
   onDeleteEvent,
   onSetActiveEvent,
   onUpdateEvent,
+  onLoadEvents
 } from "../store/calendar/calendarSlice";
+import calendarApi from "../api/calendarApi";
+import { convertEventsToDateEvent } from "../helpers/convertEventsToDateEvents";
+import Swal from "sweetalert2";
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
   const { events, activeEvent } = useSelector((state) => state.calendar);
+  const { user } = useSelector((state) => state.auth);
 
   const setActiveEvent = (calendarEvent) => {
     dispatch(onSetActiveEvent(calendarEvent));
   };
 
   const startSavingEvent = async (calendarEvent) => {
-    // actualizando
-    if (calendarEvent._id) {
-      dispatch(onUpdateEvent(calendarEvent));
-    }
-    // creando
-    else {
-      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+    try{
+      // actualizando
+      if (calendarEvent.id) {
+        await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent);
+        dispatch(onUpdateEvent(calendarEvent));
+        return;
+      }
+      
+      // creando
+      const { data } = await calendarApi.post('/events', calendarEvent);
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }));
+    } catch(error){
+      Swal.fire('Error al guardar ', error.response.data.msg, 'error');
     }
   };
 
   const startDeletingEvent = async () => {
-    dispatch(onDeleteEvent());
+    try{
+      await calendarApi.delete(`/events/${activeEvent.id}`);
+      dispatch(onDeleteEvent());
+    }catch(error){
+      Swal.fire('Error al eliminar ', error.response.data.msg, 'error');
+    }
   };
+
+  const startLoadingEvent = async () => {
+    try{
+      const { data } = await calendarApi.get('/events');
+      const events = convertEventsToDateEvent(data.eventos);
+      dispatch(onLoadEvents(events));
+    } catch(error){
+      console.log('Error cargando eventos');
+      console.log(error)
+    }
+  }
 
   return {
     events,
     activeEvent,
     hasEventSelected: !!activeEvent,
     setActiveEvent,
+    startLoadingEvent,
     startSavingEvent,
     startDeletingEvent,
   };
